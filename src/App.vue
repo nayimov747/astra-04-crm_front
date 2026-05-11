095fae03cebbe5e0394f80287c6ac6e83d84a982
nayimov747 <nayimov747@gamil.com>
2026-05-11 13:46:07 +0500

    init project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Button from "primevue/button"
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <Button label="Click" />
+</template>
+
+<style scoped></style>
